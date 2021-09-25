edffb3f47d02d2367a11eb79c4597fa5b08833ab
heohyeonjin <hyeonjin4870@naver.com>
2021-09-25 19:39:22 +0900

    add password security

--- src/main/java/com/example/hanium/repository/UserRepository.java
+++ src/main/java/com/example/hanium/repository/UserRepository.java
@@ -3,10 +3,8 @@ package com.example.hanium.repository;
 import com.example.hanium.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
-
 
 }
