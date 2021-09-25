661c4e9470305cc5f9fcfb6f7f4b5ffdc9989d76
idoburnish <doyun7433@naver.com>
2021-09-25 23:08:26 +0900

    complete login implement

--- src/main/java/com/example/hanium/controller/UserController.java
+++ src/main/java/com/example/hanium/controller/UserController.java
@@ -6,7 +6,7 @@ import com.example.hanium.dto.SignupRequestDto;
 import com.example.hanium.model.User;
 import com.example.hanium.repository.UserRepository;
 import com.example.hanium.service.UserService;
-import com.example.hanium.utils.ApiUtils;
+import com.example.hanium.utils.ApiUtils.ApiResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,16 +25,18 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping("/user/signup")
-    public List<User> checkUsers() {return userRepository.findAll();}
+    public List<User> checkUsers() {
+        return userRepository.findAll();
+    }
 
     @PostMapping("/user/signup")
-   public String registerUser(@RequestBody SignupRequestDto requestDto) {
+    public String registerUser(@RequestBody SignupRequestDto requestDto) {
         userService.registerUser(requestDto);
         return requestDto.getName();
     }
 
     @PostMapping("/user/login")
-    public ApiUtils.ApiResult<LoginSuccessDto> loginUser(@RequestBody SignInRequestDto requestDto) {
+    public ApiResult<LoginSuccessDto> loginUser(@RequestBody SignInRequestDto requestDto) {
         User user = userService.loginUser(requestDto);
 
         LoginSuccessDto lsd = new LoginSuccessDto(user.getId(), user.getUsername());
