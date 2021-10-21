4e7d2acce2828bc97f70ddc80da3a7807424d621
idoburnish <doyun7433@naver.com>
2021-10-22 03:07:11 +0900

    LoginSuccessDto 구조 변환

--- src/main/java/com/example/hanium/Auth/dto/LoginSuccessDto.java
+++ src/main/java/com/example/hanium/Auth/dto/LoginSuccessDto.java
@@ -1,14 +1,17 @@
 package com.example.hanium.Auth.dto;
 
+import com.example.hanium.Auth.model.UserLanguage;
 import lombok.Data;
 
 @Data
 public class LoginSuccessDto {
     private Long userId;
     private String userName;
+    private UserLanguage userLanguage;
 
-    public LoginSuccessDto(Long userId, String userName) {
+    public LoginSuccessDto(Long userId, String userName, UserLanguage language) {
         this.userId = userId;
         this.userName = userName;
+        this.userLanguage = language;
     }
 }
